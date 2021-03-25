042461ae23b765be62bcfba882a0b712516fcb3b
fanfanafankianki <kudlatyicss@gmail.com>
2021-03-26 00:52:20 +0100

    Added Page Factory

--- LichessSiteTests/src/test/java/pages/LoginPage.java
+++ LichessSiteTests/src/test/java/pages/LoginPage.java
@@ -2,37 +2,47 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-	
+
 	protected WebDriver driver;
-	By txt_username = By.id("form3-username");
-	By txt_password = By.id("form3-password");
-	By btn_login = By.xpath("//form[1]/div[1]/button");
-	
+
+	@FindBy(id="form3-username")
+	WebElement txt_username;
+
+	@FindBy(id="form3-password")
+	WebElement txt_password;
+
+	@FindBy(xpath="//form[1]/div[1]/button")
+	WebElement btn_login;
+
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 		if(!driver.getCurrentUrl().equals("https://lichess.org/login?referrer=/")) {
 			throw new IllegalStateException("This is not login page! The current page is" + driver.getCurrentUrl());
 		}
-		
+
 	}
-	
+
 	public void enterUsername(String username) {
-		driver.findElement(txt_username).sendKeys(username);
+		txt_username.sendKeys(username);
 	}
-	
+
 	public void enterPassword(String password) {
-		driver.findElement(txt_password).sendKeys(password);
+		txt_password.sendKeys(password);
 	}
-	
+
 	public void clickLogin() {
-		driver.findElement(btn_login).click();
+		btn_login.click();
 	}
-	
+
 	public void loginValidUser(String username, String password) {
-		driver.findElement(txt_username).sendKeys(username);
-		driver.findElement(txt_password).sendKeys(password);
-		driver.findElement(btn_login).click();
+		txt_username.sendKeys(username);
+		txt_password.sendKeys(password);
+		btn_login.click();
 	}
 }
